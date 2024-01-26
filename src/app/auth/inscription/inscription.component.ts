34013d7cc24996387e4215166b47fe1ac45421aa
saloum45 <saloumfall45@gmail.com>
2024-01-26 10:55:05 +0000

    in progress

--- src/app/auth/inscription/inscription.component.ts
+++ src/app/auth/inscription/inscription.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AllservicesService } from '../../services/allservices.service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { Commerçant } from '../../models/commerçant';
 
@@ -27,35 +27,28 @@ export class InscriptionComponent {
   public pass="";
   public email="";
   public user: any;
-  
+
   // Methodes
-  constructor(private service:AllservicesService){
+  constructor(private service:AllservicesService, private router:Router){
 
   }
   // la fonction qui permet d'inscrire un utilisateur
   inscription() {
-    if (this.nom == "" ) {
-
-      // alert('renseigner les champs');
+    this.router.navigate(['/connexion']);
+    if (this.nom == "" || this.prenom=="" || this.naissance||  this.naissance || this.genre=="" || this.nin=="" || this.ninea=="" || this.adresse=="" || this.numero=="" || this.pass=="" || this.email=="") {
       this.service.message("Désolé", "error", "Veuillez renseigner tous les champs");
     } else {
-      // this.user = new User(this.nom, this.prenom, this.naissance, this.email, this.pass,)
-      // alert('inscription okay');
-      // this.user=new Commerçant(this.nom,this.prenom,this.email,this.pass,this.numero,this.nin,this.ninea,this.adresse,this.genre,this.naissance);
       this.user= new Commerçant(this.nom,this.prenom,this.email,this.pass,this.numero,this.nin,this.ninea,this.adresse,this.genre,this.naissance);
-
       console.log("user",this.user);
       this.service.post('api/registerCommercant', this.user, (reponse: any) => {
         if (reponse.status == 200) {
-          console.log('success',reponse);
-          this.service.message("Merci!!!", "success", "Inscription faite avec succès");
+          // console.log('success',reponse);
+          this.service.message("Merci!!!", "success", "Inscription faite avec succès,Veuillez vous connecter");
         } else {
-          console.log('error ',reponse);
-          this.service.message("Désolé!!!", "error", "Inscription a échouée");
+          // console.log('error ',reponse);
+          this.service.message("Désolé!!!", "error", "Inscription a échouée, vérifier la saisie ");
         }
       });
-
-
     }
   }
 }
