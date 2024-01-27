bed816699c9126ce3688d49ebd8bc59e417c78bc
saloum45 <saloumfall45@gmail.com>
2024-01-27 18:30:43 +0000

    in progress

--- src/app/components/catalogue/catalogue.component.ts
+++ src/app/components/catalogue/catalogue.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { AllservicesService } from '../../services/allservices.service';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-catalogue',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './catalogue.component.html',
   styleUrl: './catalogue.component.css'
@@ -15,6 +17,8 @@ export class CatalogueComponent implements OnInit{
  // Attributs
  public produits:any=[];
  public categories:any=[];
+ public titre="Tout";
+ public searchInput="";
 
 
  // Methodes
@@ -26,21 +30,27 @@ export class CatalogueComponent implements OnInit{
   this.loadAllProducts();
    this.service.get("api/categories",(reponse:any)=>{
     this.categories=reponse.data;
-    console.log(reponse.data);
+    // console.log(reponse.data);
   });
  }
 
  loadAllProducts(){
   this.service.get("api/produits",(reponse:any)=>{
     this.produits=reponse.data;
-    console.log(reponse.data);
+    // console.log(reponse.data);
+    this.titre="Tout";
   });
  }
 
- getProductByCategorie(id:any){
+ getProductByCategorie(id:any,nom_categorie:any){
   this.service.get("api/produits/"+id,(reponse:any)=>{
     this.produits=reponse.data;
-    console.log("prod: ",id,reponse.data);
+    this.titre=nom_categorie;
+    // console.log("prod: ",id,reponse.data);
   });
+ }
+
+ search(){
+    return this.produits.filter((prod:any)=>prod.nom_produit.toLowerCase().includes(this.searchInput.toLowerCase()));
  }
 }
