55b95507f93297f59f083a7ab988f7d0f7cebb42
saloum45 <saloumfall45@gmail.com>
2024-01-28 22:48:09 +0000

    in progress

--- src/app/components/produit/list-produit/list-produit.component.ts
+++ src/app/components/produit/list-produit/list-produit.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AllservicesService } from '../../../services/allservices.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-produit',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './list-produit.component.html',
   styleUrl: './list-produit.component.css'
 })
-export class ListProduitComponent {
+export class ListProduitComponent implements OnInit {
+  // Attributs
+  public produits: any = [];
+  public searchInput = "";
 
+
+  // Methodes
+  constructor(private service: AllservicesService) {
+
+  }
+  ngOnInit(): void {
+    this.service.get("api/getProduitsByCommercant", (reponse: any) => {
+      console.log(reponse);
+      this.produits = reponse.data;
+    });
+  }
+
+  search(){
+    return this.produits.filter((prod:any)=>prod.nom_produit.toLowerCase().includes(this.searchInput.toLowerCase()));
+ }
 }
