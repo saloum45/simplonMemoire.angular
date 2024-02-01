cf0311d162c9a8277a0bbb78e2968029ef6f9d96
saloum45 <saloumfall45@gmail.com>
2024-02-01 10:40:17 +0000

    in progress

--- src/app/components/admin/sidebar/sidebar.component.ts
+++ src/app/components/admin/sidebar/sidebar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
     RouterOutlet,
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
