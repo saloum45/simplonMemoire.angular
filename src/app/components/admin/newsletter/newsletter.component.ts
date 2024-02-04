ac6a695224397cd2f6e8bf7a28a482675c5d1f41
saloum45 <saloumfall45@gmail.com>
2024-02-04 16:43:38 +0000

    in progress

--- src/app/components/admin/newsletter/newsletter.component.ts
+++ src/app/components/admin/newsletter/newsletter.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AllservicesService } from '../../../services/allservices.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './newsletter.component.html',
   styleUrl: './newsletter.component.css'
 })
-export class NewsletterComponent {
+export class NewsletterComponent implements OnInit{
+  // Attributs
+  public letter="";
 
+  // Methodes
+  constructor(private service:AllservicesService){
+
+  }
+  ngOnInit(): void {
+    // this.informer();
+  }
+
+  informer(){
+    alert('test');
+    this.service.post('api/envoyerMail',{letter:this.letter},((reponse:any)=>{
+      console.log(reponse);
+    }));
+  }
 }
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,29 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { AllservicesService } from '../../services/allservices.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent implements OnInit{
+export class FooterComponent implements OnInit {
   // Attributs
-
+  public email = "";
 
 
 
 
   // Methodes
-  constructor(private service:AllservicesService){
+  constructor(private service: AllservicesService) {
 
   }
   ngOnInit(): void {
 
   }
 
-  newsletterSubscribe(){
-    this.service.message('Oops','warning','Cette fonctionnalité n\'est encore disponible')
+  newsletterSubscribe() {
+    if (this.email == "") {
+      this.service.message("OOps", "error", "Veuillez renseigner le champs");
+    } else {
+
+      this.service.post('api/inscriptionNewsletter', { email: this.email }, ((reponse: any) => {
+        console.log(reponse);
+      }));
+    }
   }
+
+
 }
