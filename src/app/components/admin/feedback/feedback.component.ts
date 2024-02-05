bd174d69867fb7c928aeb2bb3d264cb9da7d918f
saloum45 <saloumfall45@gmail.com>
2024-02-05 15:25:17 +0000

    in progress

--- src/app/components/admin/feedback/feedback.component.ts
+++ src/app/components/admin/feedback/feedback.component.ts
@@ -1,19 +1,48 @@
 import { Component } from '@angular/core';
+import { AllservicesService } from '../../../services/allservices.service';
+import { DataTablesModule } from 'angular-datatables';
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [],
+  imports: [
+    DataTablesModule
+  ],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.css'
 })
 export class FeedbackComponent {
   // Attributs
+  dtOptions: DataTables.Settings = {};
+  public feedbacks:any[]=[];
+  public reponse="";
+
+
 
 
 
   // Methodes
-envoyer(){
-  
-}
+  constructor(private service:AllservicesService){
+
+  }
+  ngOnInit(): void {
+    this.dtOptions = {
+      searching: true,
+      lengthChange: false,
+      paging: true,
+      info: false,
+      pageLength: 7,
+      language: {
+        url: 'https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json',
+      }
+    };
+    this.loadAllFeedbakcs();
+  }
+
+  loadAllFeedbakcs(){
+    this.service.get("api/ListeFeedback",((reponse:any)=>{
+      this.feedbacks=reponse.data;
+      console.log(this.feedbacks);
+    }));
+  }
 }
