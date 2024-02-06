989ca97b4e4f372d1c4167eed1022fb798561fe7
saloum45 <saloumfall45@gmail.com>
2024-02-06 16:57:17 +0000

    in progress

--- src/app/components/admin/livraison/livraison.component.ts
+++ src/app/components/admin/livraison/livraison.component.ts
@@ -1,82 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { AllservicesService } from '../../../services/allservices.service';
 import { DataTablesModule } from 'angular-datatables';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-livraison',
   standalone: true,
   imports: [
-    DataTablesModule
+    DataTablesModule,
+    DatePipe
   ],
   templateUrl: './livraison.component.html',
   styleUrl: './livraison.component.css'
 })
 export class LivraisonComponent implements OnInit {
   // Attributs
-dtOptions: DataTables.Settings = {};
-public livreurs:any[]=[];
-  public commandes: any[] = [
-    {
-      date_commande: "14-06-2024",
-      adresse: "Almadies",
-      etat: "en cours"
-    },
+  public nom = "";
+  public numero = "";
+  public adresse = "";
 
-    {
-      date_commande: "03-11-2024",
-      adresse: "Mermoz",
-      etat: "en attente"
-    },
-
-    {
-      date_commande: "20-09-2024",
-      adresse: "Point E",
-      etat: "terminée"
-    },
-
-    {
-      date_commande: "08-04-2024",
-      adresse: "Yoff",
-      etat: "en cours"
-    },
-
-    {
-      date_commande: "17-07-2024",
-      adresse: "Parcelles Assainies",
-      etat: "en attente"
-    },
-
-    {
-      date_commande: "29-01-2024",
-      adresse: "Hann Bel-Air",
-      etat: "terminée"
-    },
-
-    {
-      date_commande: "12-10-2024",
-      adresse: "Diamniadio",
-      etat: "en cours"
-    },
-
-    {
-      date_commande: "05-02-2024",
-      adresse: "Thiaroye",
-      etat: "en attente"
-    },
-
-    {
-      date_commande: "23-05-2024",
-      adresse: "Guédiawaye",
-      etat: "terminée"
-    },
-
-    {
-      date_commande: "01-12-2024",
-      adresse: "Ouest Foire",
-      etat: "en cours"
-    }
-  ];
-
+  dtOptions: DataTables.Settings = {};
+  public livreurs: any[] = [];
+  public commandes: any[] = [];
 
   // Methodes
   constructor(private service: AllservicesService) {
@@ -100,13 +45,23 @@ public livreurs:any[]=[];
   loadAllCommands() {
     this.service.get('api/commandes', ((reponse: any) => {
       console.log(reponse);
+      this.commandes = reponse.data;
     }));
   }
 
-  getLivreurs(){
-    this.service.get("api/ListerLivreur",((reponse:any)=>{
+  getLivreurs() {
+    this.service.get("api/ListerLivreur", ((reponse: any) => {
       console.log(reponse);
-      this.livreurs=reponse.data;
+      this.livreurs = reponse.data;
+    }));
+  }
+
+  showCommande(id: any) {
+    this.service.get('api/Detailscommandes/' + id, ((reponse: any) => {
+      console.warn(reponse);
+      this.nom=reponse.data.nom_client;
+      this.adresse=reponse.data.adresse_client;
+      this.numero=reponse.data.numero_tel;
     }));
   }
 }
