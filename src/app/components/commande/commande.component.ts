f746e2b165f33faafbb361c7bd3200ab07bc857f
saloum45 <saloumfall45@gmail.com>
2024-02-16 15:59:00 +0000

    okay

--- src/app/components/commande/commande.component.ts
+++ src/app/components/commande/commande.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { DataTablesModule } from 'angular-datatables';
 import { AllservicesService } from '../../services/allservices.service';
 import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-commande',
   standalone: true,
   imports: [
     DataTablesModule,
-    DatePipe
+    DatePipe,
+    RouterLink
   ],
   templateUrl: './commande.component.html',
   styleUrl: './commande.component.css'
@@ -18,6 +20,8 @@ export class CommandeComponent implements OnInit {
   // Attributs
   dtOptions: DataTables.Settings = {};
   public commandes: any[] = [];
+  public produitCommandes: any[] = [];
+  public urlBaseImage = this.service.urlBaseImage;
 
   // Methodes
   constructor(private service: AllservicesService) {
@@ -36,8 +40,19 @@ export class CommandeComponent implements OnInit {
     };
     this.service.get('api/listeCommande', ((reponse: any) => {
       console.log(reponse);
-      this.commandes=reponse.data;
+      this.commandes = reponse.data;
     }));
 
   }
+
+  showVente(id: number) {
+    this.service.get("api/showCommande/" + id, ((reponse: any) => {
+      console.warn(reponse);
+      this.produitCommandes = reponse.data;
+    }));
+  }
+
+  postPanier(produit:any){
+    this.service.postToPanier(produit);
+   }
 }
