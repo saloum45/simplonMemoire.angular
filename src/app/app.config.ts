9222bb9d3bfee386f7db36bfd9a3a2d29799c39e
saloum45 <saloumfall45@gmail.com>
2024-02-19 16:34:59 +0000

    local date format okay

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+// rendre la date en format français
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr, 'fr');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     {provide:LocationStrategy,useClass:HashLocationStrategy},
-    provideClientHydration(),provideHttpClient()
+    provideClientHydration(),provideHttpClient(),
+    {provide: LOCALE_ID, useValue: 'fr' }
   ]
 };
