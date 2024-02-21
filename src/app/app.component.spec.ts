0006281ad29e44544893f6737b999df797dc0649
saloum45 <saloumfall45@gmail.com>
2024-02-21 19:01:23 +0000

    test in progress

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(true).toBeTruthy(true);
+    expect(true).toBeTruthy();
   });
 });
--- src/app/components/admin/sidebar/sidebar.component.spec.ts
+++ src/app/components/admin/sidebar/sidebar.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidebarComponent } from './sidebar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
@@ -9,7 +10,7 @@ describe('SidebarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SidebarComponent,HttpClientModule]
+      imports: [SidebarComponent,HttpClientModule,RouterTestingModule]
     })
     .compileComponents();
 
