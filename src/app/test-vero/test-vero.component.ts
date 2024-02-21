7fe152cbc784d50096d46d800e2a17fe49c38cdd
saloum45 <saloumfall45@gmail.com>
2024-02-21 16:48:32 +0000

    okay

--- src/app/test-vero/test-vero.component.ts
+++ src/app/test-vero/test-vero.component.ts
@@ -1,17 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test-vero',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './test-vero.component.html',
   styleUrl: './test-vero.component.css'
 })
 export class TestVeroComponent {
-// Attributs
-public isSecondCrenaux=false;
+  // Attributs
+  public isSecondCrenaux = false;
+  public plannings: any[] = [];
+  public planningsObjet: any = {
+    jour: "",
+    creanaux: [
+      {
+        startTime: "",
+        endTime: ""
+      }
+    ]
+  };
 
- addCrenaux(){
-  this.isSecondCrenaux=!this.isSecondCrenaux;
- }
+  addCrenaux() {
+    this.isSecondCrenaux = !this.isSecondCrenaux;
+  }
+  addPlannings() {
+    //  throw new Error('Method not implemented.');
+    this.plannings.push(this.planningsObjet);
+    console.log(this.plannings)
+
+  }
+
+  selectJour(event: any) {
+    this.planningsObjet.jour = event.target.value;
+  }
 }
+
