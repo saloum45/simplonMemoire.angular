87e74ea1be22deff3c4364d3e2f7ac387e4fb221
saloum45 <saloumfall45@gmail.com>
2024-02-21 09:58:36 +0000

    okay

---
+++ src/app/test-vero/test-vero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-vero',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-vero.component.html',
+  styleUrl: './test-vero.component.css'
+})
+export class TestVeroComponent {
+
+}
